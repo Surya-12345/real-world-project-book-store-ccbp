cc64c993c650b22d47df6146812d57b0897a15fe
Surya-12345 <suryateja.alamuri4@gmail.com>
2023-12-13 10:54:40 +0530

    Added routes to the Application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import Books from "./components/Books";
-import LoginForm from "./components/LoginForm";
+import BookList from "./components/BookList"
+import BookDetails from "./components/BookDetails"
+import Cart from "./components/Cart"
+import Checkout from"./components/Checkout"
+import NotFound from "./components/NotFound"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/login" Component={LoginForm} />
         <Route exact path="/" Component={Home} />
-        <Route exact path="/books" Component={Books} />
+        <Route exact path="/books" Component={BookList} />
+        <Route exact path="/books/:id" Component={BookDetails} />
+        <Route exact path="/cart" Component={Cart} />
+        <Route exact path="/checkout" Component={Checkout} />
+        <Route exact path="/not-found" Component={NotFound} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>hi ra</div>
+  )
+}
+
+export default NotFound
